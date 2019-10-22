8336294dd73966d308b138b7adf8c9ca2b48de66
canisterism <ohtthstc@gmail.com>
2019-10-23 01:02:47 +0900

    yarn add react-helmet @types/react-helmet

--- src/components/Setsu/index.tsx
+++ src/components/Setsu/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Button, Header } from 'semantic-ui-react';
+import Helmet from 'react-helmet';
 
 export type SetsuProps = {
   src: string;
@@ -8,6 +9,12 @@ export type SetsuProps = {
 const Setsu: FC<SetsuProps> = ({ src }) => {
   return (
     <>
+      <Helmet
+        meta={[
+          { property: 'og:image', content: src },
+          { property: 'og:url', content: src } // TODO
+        ]}
+      />
       <Button>DO IT</Button>
       <Header>id: {src}</Header>
       <img src={src} alt='' />
