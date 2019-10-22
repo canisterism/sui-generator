aa146c1b3ec75cc1b0db634f72eefe92416352ce
canisterism <ohtthstc@gmail.com>
2019-10-22 17:02:18 +0900

    [add] handle error

--- src/utils/firebase/storage.ts
+++ src/utils/firebase/storage.ts
@@ -3,7 +3,15 @@ import { firebaseStorage } from './index';
 export const uploadBase64 = (
   src: string,
   filename: string
-): Promise<string> => {
-  const ref = firebaseStorage.child(filename);
-  return ref.putString(src).then(snapShot => snapShot.metadata.fullPath);
+): Promise<string> | Promise<Error> => {
+  const ref = firebaseStorage.child(filename.split('\n').join(''));
+  return ref
+    .putString(src, 'data_url')
+    .then(snapShot => {
+      return snapShot.metadata.fullPath;
+    })
+    .catch(e => {
+      console.log(e);
+      throw new Error();
+    });
 };
