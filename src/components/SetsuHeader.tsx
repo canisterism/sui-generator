0ec6735902596c7ad664b35f3a96d99b2523acec
canisterism <ohtthstc@gmail.com>
2019-10-26 21:34:04 +0900

    [add] link to top

--- src/components/SetsuHeader.tsx
+++ src/components/SetsuHeader.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import { Menu, Container, Image } from 'semantic-ui-react';
 import setsu from '../styles/theme/default/assets/images/setsu.svg';
+import { Link } from 'react-router-dom';
 const style = {
   menu: {
     borderRadius: '0'
   },
+  link: { display: 'inline-flex' },
   image: {
     margin: '0.5rem 0.5rem 0.5rem 1.5rem '
   },
@@ -20,8 +22,10 @@ const SuiHeader: FC = () => {
     <>
       <Menu inverted style={style.menu}>
         <Container>
-          <Image size='mini' src={setsu} style={style.image}></Image>
-          <Menu.Item style={style.menuTitle}>「水」ジェネレーター</Menu.Item>
+          <Link to='/' style={style.link}>
+            <Image size='mini' src={setsu} style={style.image}></Image>
+            <Menu.Item style={style.menuTitle}>「水」ジェネレーター</Menu.Item>
+          </Link>
         </Container>
       </Menu>
     </>
